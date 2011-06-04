0c1e1ad2c236f02bad95d22151eebce9d7cee6d2
aldolat <aldolat@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-06-04 13:27:48 +0000

    Tagging version 2.4
    git-svn-id: https://plugins.svn.wordpress.org/delicious-wishlist-for-wordpress/tags/2.4@393040 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ uninstall.php
@@ -0,0 +1,14 @@
+<?php
+/*
+ * Delicious Wishlist for WordPress uninstallation
+ *
+ * @since 0.5
+ */
+
+
+// Check for the 'WP_UNINSTALL_PLUGIN' constant, before executing
+if( !defined('ABSPATH' ) && !defined( 'WP_UNINSTALL_PLUGIN' ) )
+	exit();
+
+// Delete options from the database
+delete_option('wdw_options');
